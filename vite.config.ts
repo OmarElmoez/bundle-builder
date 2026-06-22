0cfc21495be29cd14abb61ac022a61208be2ebbc
OmarElmoez <omarelmoez5@gmail.com>
2026-06-22 23:47:28 +0300

    install the missing dependancy postcss and configure it

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import path from 'path'
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(fileURLToPath(new URL(".", import.meta.url)), "src"),
     },
   },
 });
