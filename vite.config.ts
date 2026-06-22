716d6990e6b81a14c3d7acf63b74b18a66e6bcd3
OmarElmoez <omarelmoez5@gmail.com>
2026-06-22 23:56:43 +0300

    edit alias at vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
+import path from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(fileURLToPath(new URL(".", import.meta.url)), "src"),
-    },
+			"@": path.resolve(__dirname, "./src"),
+		},
   },
 });
